a5e50944f3f1be4c2f7e01e15019b39411622bb7
Frankietime <donadiofranco7@gmail.com>
2015-01-01 23:21:01 -0300

    Initial

---
+++ Frank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Frank.Startup))]
+namespace Frank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
